2b61b2b383baa794e0abdb7d8cb991a19fd26ccb
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2020-10-08 09:28:03 +0000

    Pull a lever function added

--- tile_traveller.py
+++ tile_traveller.py
@@ -37,29 +37,40 @@ def print_directions(directions_str):
           first = False
      print(".")
 
-def find_directions(col, row):
+
+def pull_a_lever(coin):
+     user_input = input("Pull a lever(y/n): ")
+
+     if user_input == 'y':
+          coin += 1
+          return print("You received 1 coin, your total is now", coin)
+
+def find_directions(col, row, coin):
      ''' Returns valid directions as a string given the supplied location '''
      if col == 1 and row == 1:   # (1,1)
           valid_directions = NORTH
      elif col == 1 and row == 2:  # (1,2)
+          pull_a_lever(coin)
           valid_directions = NORTH+EAST+SOUTH
      elif col == 1 and row == 3:  # (1,3)
           valid_directions = EAST+SOUTH
      elif col == 2 and row == 1:  # (2,1)
           valid_directions = NORTH
      elif col == 2 and row == 2:  # (2,2)
+          pull_a_lever(coin)
           valid_directions = SOUTH+WEST
      elif col == 2 and row == 3:  # (2,3)
+          pull_a_lever(coin)
           valid_directions = EAST+WEST
      elif col == 3 and row == 2:  # (3,2)
+          pull_a_lever(coin)
           valid_directions = NORTH+SOUTH
      elif col == 3 and row == 3:  # (3,3)
           valid_directions = SOUTH+WEST
      return valid_directions
 
 def play_one_move(col, row, valid_directions):
-     ''' Plays one move of the game
-          Return if victory has been obtained and updated col,row '''
+     ''' Plays one move of the game. Return if victory has been obtained and updated col,row '''
      victory = False
      direction = input("Direction: ")
      direction = direction.lower()
@@ -75,9 +86,10 @@ def play_one_move(col, row, valid_directions):
 victory = False
 row = 1
 col = 1
+coin = 0
 
 while not victory:
-     valid_directions = find_directions(col, row)
+     valid_directions = find_directions(col, row, coin)
      print_directions(valid_directions)
      victory, col, row = play_one_move(col, row, valid_directions)
 print("Victory!")
